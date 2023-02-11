e59d3a4fce9d226f5e74d690f288736fa8d6c940
MohamedBassem <medoox240@gmail.com>
2023-02-11 22:25:36 +0000

    Add the trigger protobuf def

--- proto/build.rs
+++ proto/build.rs
@@ -1,5 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("../proto/scheduler.proto")?;
     tonic_build::compile_protos("../proto/dispatcher.proto")?;
+    tonic_build::compile_protos("../proto/trigger.proto")?;
     Ok(())
 }
--- proto/lib.rs
+++ proto/lib.rs
@@ -5,3 +5,7 @@ pub mod scheduler_proto {
 pub mod dispatcher_proto {
     tonic::include_proto!("dispatcher_proto");
 }
+
+pub mod trigger_proto {
+    tonic::include_proto!("trigger_proto");
+}
