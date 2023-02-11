6aa3d99671b83737787b911234f2375534133ccf
Ahmed Farghal <me@asoli.dev>
2023-02-11 20:56:37 +0000

    empty service interface

--- scheduler/handler.rs
+++ scheduler/handler.rs
@@ -1,6 +1,10 @@
 use std::{sync::Arc, time::Duration};
 
-use proto::scheduler_proto::{scheduler_server::Scheduler, EchoRequest, EchoResponse};
+use proto::scheduler_proto::{
+    scheduler_server::Scheduler, ExecuteTriggerRequest, ExecuteTriggerResponse,
+    FindTriggersRequest, FindTriggersResponse, GetTriggerRequest, GetTriggerResponse,
+    InstallTriggerRequest, InstallTriggerResponse, UpdateTriggerRequest, UpdateTriggerResponse,
+};
 use tonic::{Request, Response, Status};
 
 use shared::config::ConfigLoader;
@@ -12,14 +16,43 @@ pub(crate) struct SchedulerAPIHandler {
 
 #[tonic::async_trait]
 impl Scheduler for SchedulerAPIHandler {
-    async fn echo(&self, request: Request<EchoRequest>) -> Result<Response<EchoResponse>, Status> {
+    async fn install_trigger(
+        &self,
+        request: Request<InstallTriggerRequest>,
+    ) -> Result<Response<InstallTriggerResponse>, Status> {
         info!("Got a request: {request:?}");
 
         tokio::time::sleep(Duration::from_millis(800)).await;
-        let reply = EchoResponse {
-            name: format!("Hello {}!", request.into_inner().name),
-        };
+        let reply = InstallTriggerResponse {};
 
         Ok(Response::new(reply))
+    }
+
+    async fn update_trigger(
+        &self,
+        request: Request<UpdateTriggerRequest>,
+    ) -> Result<Response<UpdateTriggerResponse>, Status> {
+        todo!()
+    }
+
+    async fn execute_trigger(
+        &self,
+        request: Request<ExecuteTriggerRequest>,
+    ) -> Result<Response<ExecuteTriggerResponse>, Status> {
+        todo!()
+    }
+
+    async fn get_trigger(
+        &self,
+        request: Request<GetTriggerRequest>,
+    ) -> Result<Response<GetTriggerResponse>, Status> {
+        todo!()
+    }
+
+    async fn find_triggers(
+        &self,
+        request: Request<FindTriggersRequest>,
+    ) -> Result<Response<FindTriggersResponse>, Status> {
+        todo!()
     }
 }
